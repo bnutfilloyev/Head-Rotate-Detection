0d2025b9aea83b7eab1bfc45387e7a22891e3b4a
Bekhruz Nutfilloyev <yoshlikmedia@gmail.com>
2022-06-08 15:29:26 +0500

    Checker method changed

--- handlers/users/get_photos.py
+++ handlers/users/get_photos.py
@@ -15,18 +15,18 @@ text_template = "<b>Result: </b>{}" \
 
 
 # dlib detector
-@dp.message_handler(content_types=ContentType.PHOTO)
-async def get_photo(msg: Message):
-    photo = await msg.photo[-1].download("photos/{}.jpg".format(msg.photo[-1].file_id))
-    image = cv2.imread(photo.name)
-    result, pose, flag = image_main(image, image.shape[0], image.shape[1])
-    image = cv2.imencode(".jpg", result)[1].tobytes()
-    await msg.answer_photo(image, caption=text_template.format(flag, pose[0], pose[1]))
-    # await bot.send_photo(CHANEL, image, caption=text_template.format(flag, pose[0], pose[1]))
-    await msg.answer("<b>Rotation Vector Sum:</b>"
-                     "\n<code>{rotation}</code>"
-                     "\n\n<b>Translation Vector Sum:</b>"
-                     "\n<code>{translation}</code>".format(rotation=np.sum(pose[0]), translation=np.sum(pose[1])))
+# @dp.message_handler(content_types=ContentType.PHOTO)
+# async def get_photo(msg: Message):
+#     photo = await msg.photo[-1].download("photos/{}.jpg".format(msg.photo[-1].file_id))
+#     image = cv2.imread(photo.name)
+#     result, pose, flag = image_main(image, image.shape[0], image.shape[1])
+#     image = cv2.imencode(".jpg", result)[1].tobytes()
+#     await msg.answer_photo(image, caption=text_template.format(flag, pose[0], pose[1]))
+#     # await bot.send_photo(CHANEL, image, caption=text_template.format(flag, pose[0], pose[1]))
+#     await msg.answer("<b>Rotation Vector Sum:</b>"
+#                      "\n<code>{rotation}</code>"
+#                      "\n\n<b>Translation Vector Sum:</b>"
+#                      "\n<code>{translation}</code>".format(rotation=np.sum(pose[0]), translation=np.sum(pose[1])))
 
 
 @dp.message_handler(content_types=ContentType.PHOTO)
